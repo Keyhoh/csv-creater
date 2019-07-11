63d6f89b780a54b45ea4bdd1baa35392835af2cb
Keyhoh <iheyhoh@gmail.com>
2019-07-11 23:03:22 +0900

    fix: correspond NaN

--- src/main/executable/sequencer.js
+++ src/main/executable/sequencer.js
@@ -1,4 +1,8 @@
 module.exports.sequencer = (from = 1, step = 1) => {
+
+    from = isNaN(from = Number(from)) ? 1 : from;
+    step = isNaN(step = Number(step)) ? 1 : step;
+
     const seq = () => {
         seq.counter += step;
         return seq.counter;
