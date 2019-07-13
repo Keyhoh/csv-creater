57041c12388fe5d03e8ff26a3623fd7f6e92891b
Keyhoh <iheyhoh@gmail.com>
2019-07-13 22:13:30 +0900

    clean: create interface for random generator

--- src/main/executable/randomSwitcher.js
+++ src/main/executable/randomSwitcher.js
@@ -1,3 +1,7 @@
 const randomSwitcher = (executable1 = () => null, executable2 = () => null, rate = 0.5) => {
-    return () => Math.random() < rate ? executable1() : executable2();
+    return () => randomSwitcher.create() < rate ? executable1() : executable2();
 }
+
+randomSwitcher.create = () => Math.random();
+
+module.exports.randomSwitcher = randomSwitcher;
