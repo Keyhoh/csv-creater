4a91ea5e17ec0d0633a1fd86dfd9584a37d0da0b
Keyhoh <iheyhoh@gmail.com>
2019-07-13 17:26:03 +0900

    clean: change counter to increment

--- src/main/executable/sequencer.js
+++ src/main/executable/sequencer.js
@@ -3,10 +3,8 @@ module.exports.sequencer = (from = 1, step = 1) => {
     from = isNaN(from = Number(from)) ? 1 : from;
     step = isNaN(step = Number(step)) ? 1 : step;
 
-    const seq = () => {
-        seq.counter += step;
-        return seq.counter;
-    }
-    seq.counter = from - step;
+    const seq = () => from + step * seq.counter++;
+
+    seq.counter = 0;
     return seq;
 }
