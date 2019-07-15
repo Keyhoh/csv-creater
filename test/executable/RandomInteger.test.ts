4290f29d624dbf554b69ae4f65d134b5ef017e5f
Keyhoh <iheyhoh@gmail.com>
2019-07-15 13:23:43 +0900

    fix: change to return 0.99 because random: () -> [0,1)

--- test/executable/RandomInteger.test.ts
+++ test/executable/RandomInteger.test.ts
@@ -20,20 +20,20 @@ describe('RandomInteger', () => {
         assert.equal(reverseFromTo.create(), 0);
     });
 
-    it("should return 'from + len' integer", () => {
-        const defaultRand = new RandomInteger({ from: 0, len: 1, random: () => 1 });
-        assert.equal(defaultRand.create(), 1);
+    it("should return 'len' th integer", () => {
+        const defaultRand = new RandomInteger({ from: 0, len: 1, random: () => 0.99 });
+        assert.equal(defaultRand.create(), 0);
 
-        const randFromNegative = new RandomInteger({ from: -10, len: 1, random: () => 1 });
-        assert.equal(randFromNegative.create(), -9);
+        const randFromNegative = new RandomInteger({ from: -10, len: 1, random: () => 0.99 });
+        assert.equal(randFromNegative.create(), -10);
 
-        const randFromPositive = new RandomInteger({ from: 10, len: 1, random: () => 1 });
-        assert.equal(randFromPositive.create(), 11);
+        const randFromPositive = new RandomInteger({ from: 10, len: 1, random: () => 0.99 });
+        assert.equal(randFromPositive.create(), 10);
 
-        const normalFromTo = new RandomInteger({ from: 0, len: 10, random: () => 1 });
-        assert.equal(normalFromTo.create(), 10);
+        const normalFromTo = new RandomInteger({ from: 0, len: 10, random: () => 0.99 });
+        assert.equal(normalFromTo.create(), 9);
 
-        const reverseFromTo = new RandomInteger({ from: 0, len: -10, random: () => 1 });
-        assert.equal(reverseFromTo.create(), -10);
+        const reverseFromTo = new RandomInteger({ from: 0, len: -10, random: () => 0.99 });
+        assert.equal(reverseFromTo.create(), -9);
     });
 })
