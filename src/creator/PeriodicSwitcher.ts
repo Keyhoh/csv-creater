2a4ca7b06c9f872cbc97022a54c983aa056f0378
Keyhoh <iheyhoh@gmail.com>
2019-07-16 22:09:34 +0900

    implement: add PeriodSwitcher

---
+++ src/creator/PeriodicSwitcher.ts
@@ -0,0 +1,33 @@
+import StringCreator from "./StringCreator";
+import MixStringCreator from "./MixStringCreator";
+
+export default class PeriodicSwitcher extends MixStringCreator {
+    private readonly firstPeriod: number;
+    private readonly secondPeriod: number;
+
+    private firstCounter: number = 0;
+    private secondCounter: number = 0;
+
+    constructor(
+        { firstCreator, secondCreator }: { firstCreator: StringCreator, secondCreator: StringCreator },
+        { firstPeriod, secondPeriod }: { firstPeriod: number, secondPeriod: number },
+    ) {
+        super({ firstCreator, secondCreator });
+        this.firstPeriod = firstPeriod;
+        this.secondPeriod = secondPeriod;
+    }
+
+    useFirst(): boolean {
+        return this.firstCounter > 0;
+    }
+
+    private count() {
+        if (this.firstCounter < this.firstPeriod) {
+            this.firstCounter++;
+            this.secondCounter = 0;
+        } else if (this.secondCounter < this.secondPeriod) {
+            this.secondCounter++;
+            this.firstCounter = 0;
+        }
+    }
+}
